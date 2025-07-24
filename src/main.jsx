e245489ac820dbbf0269e0254bc879d4a892dbd5
Yasmeen Mahmoud <yasmeen.mahmoud1610@gmail.com>
2025-07-25 01:11:23 +0300

    feat:Add Layout Routing With RouterLayout Component And Creating Container Class

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { Provider } from 'react-redux'
-import { store } from './Redux/store.js'
-import ThemeProvider from './Theme/ThemeProvider.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <ThemeProvider>
 
         <App />
-      </ThemeProvider>
 
-    </Provider>
   </StrictMode>,
 )
