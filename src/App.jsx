f090009ab967b65c0eeccacc85b5776aa6be730b
Yasmeen Mahmoud <yasmeen.mahmoud1610@gmail.com>
2025-07-24 23:58:30 +0300

    chore: install redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {Button} from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 export default function App() {
   return <>
-    <h1 className="bg-red-600">hi</h1>
+    <h1 className="bg-red-600 ">hi</h1>
     <Button variant="">Button</Button>
   </>
 }
