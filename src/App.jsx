765ceb46e106567b4092914bb57f65d91a5d4071
Yasmeen Mahmoud <yasmeen.mahmoud1610@gmail.com>
2025-07-24 23:13:03 +0300

    chore: install and configure shadcn/ui, react-router-dom ,axios ,lucid-react and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Button} from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+export default function App() {
+  return <>
+    <h1 className="bg-red-600">hi</h1>
+    <Button variant="">Button</Button>
+  </>
+}
